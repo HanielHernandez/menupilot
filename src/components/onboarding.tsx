deb33c9af970b2d74d7469edeb38c0316cc1ba2c
HanielHernandez <haniel@cloudspot.io>
2026-07-24 16:04:26 -0600

    feat(app): added theming

---
+++ src/components/onboarding.tsx
@@ -0,0 +1,9 @@
+export default function Onboarding() {
+  return (
+    <div className="flex flex-col items-center justify-center min-size-screen h-full">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-2xl font-bold">Onboarding</h1>
+      </div>
+    </div>
+  );
+}
